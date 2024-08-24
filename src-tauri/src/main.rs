bd913b4d674973a5cb1759bc210195fcf2c8619c
Leon Zhao <zhaoleon52@gmail.com>
2024-08-24 12:53:58 -0400

    rust test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn create_cpp_file(contents: &str) -> String {
+    // create a cpp file with the contents
+    let mut file = std::fs::File::create("temp.cpp").unwrap();
+    file.write_all(contents.as_bytes()).unwrap();
+}
+
+#[tauri::command]
+fn compile_cpp_file(path: &str) -> String {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "echo hello"])
+            .output()
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("g++ -std=c++20")
+            .arg(path)
+            .output()
+    };
+    format!("Hello, {}! You've been greeted from Rust!", path)
 }
 
 fn main() {
--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,32 @@ import "./App.css";
 
 function TextBox(props: any) {
     const [text, setText] = useState("");
-    console.log(text)
+
+    var name = "test"
+
+    async function test() {
+        var greeting = await invoke("greet", {name})
+        console.log(greeting);
+    }
+    test()
+
     return (
         <div className="m-3 flex flex-col grow">
         
             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Your message
+                {props.placeholder}
             </label>
 
             <textarea 
                 className="textarea textarea-bordered resize-none grow" 
-                placeholder="Bio"
+                placeholder={props.placeholder}
                 value = {text}
-                onChange={(e) => setText(e.target.value)}
+                onChange={
+                    (e) => {
+                        setText(e.target.value)
+                        props.setSolutionText(e.target.value)
+                    }
+                }
             />
 
         </div>
@@ -27,66 +40,38 @@ function TextBox(props: any) {
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [solution, setSolution] = useState("");
+  const [bruteSolution, setBruteSolution] = useState("");
 
-  async function greet() {
+  async function greet() { 
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    // 
     setGreetMsg(await invoke("greet", { name }));
+    console.log(greetMsg);
   }
+
+  console.log("Solution: " + solution)
 
   return (
       <div className="flex h-screen">
-
-      {
-        // <div className="m-5 bg-red-500">
-        // <h1>Welcome to Tauri!</h1>
-        // </div>
-      }
         <div className="flex w-2/3">
-
-          <TextBox />
-          <TextBox />
-
-        </div>
-
-        <button className="btn btn-outline">Stress</button>
+          <TextBox 
+            placeholder="Your solution here" 
+            solutionText={solution} 
+            setSolutionText={setSolution}
+          />
+          <TextBox 
+            placeholder="Brute force solution"
+            solutionText={bruteSolution}
+            setSolutionText={setBruteSolution}
+          />
+         </div>
+        <button 
+          className="btn btn-outline"
+          onClick={() => greet()}
+        >
+          Stress
+        </button>
       </div>
-
-    // <div className="container">
-    //   <h1>Welcome to Tauri!</h1>
-    //   <h1>Learning Stuff</h1>
-
-    //   <div className="row">
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://tauri.app" target="_blank">
-    //       <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-
-    //   <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-    //   <form
-    //     className="row"
-    //     onSubmit={(e) => {
-    //       e.preventDefault();
-    //       greet();
-    //     }}
-    //   >
-    //     <input
-    //       id="greet-input"
-    //       onChange={(e) => setName(e.currentTarget.value)}
-    //       placeholder="Enter a name..."
-    //     />
-    //     <button type="submit">Greet</button>
-    //   </form>
-
-    //   <p>{greetMsg}</p>
-    // </div>
   );
 }
 
